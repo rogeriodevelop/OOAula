2ef96455c101524523f81953f269cd0c0a0621d6
rogeriodevelop <rogeriodevelop@gmail.com>
2022-06-19 23:35:04 -0300

    criando classe para aula

---
+++ src/com/digitalinnovationone/oo/Carro.java
@@ -0,0 +1,48 @@
+package com.digitalinnovationone.oo;
+
+public class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    public Carro() {
+
+    }
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public double totalvalorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+
+    }
+
+}
